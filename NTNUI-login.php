d683e016e6aea3e5f0b6f9ec5bebe0582d73982f
Eivind Dalholt <eividalh@stud.ntnu.no>
2022-04-03 15:18:33 +0200

    initial commit

---
+++ NTNUI-login.php
@@ -0,0 +1,146 @@
+<?php
+
+function getMemberData(){
+	$url = ("https://api.ntnui.no/groups/". get_option('group_slug') . "/memberships/");
+
+	$curl = curl_init($url);
+	curl_setopt($curl, CURLOPT_URL, $url);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+	$headers = array(
+	   "API-KEY: ". get_option('api_key'),
+	);
+	curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+
+	$resp = curl_exec($curl);
+	curl_close($curl);
+	
+	return json_decode($resp, true);
+}
+
+function validLogin($phone, $password){
+	$url = ("https://api.ntnui.no/token/");
+
+	$fields = [
+		'phone_number'	=> $phone,
+		'password' 		=> $password,
+	];
+	$fields_string = http_build_query($fields);
+	$curl = curl_init();
+
+	//Post request options
+	curl_setopt($curl,CURLOPT_URL, $url);
+	curl_setopt($curl,CURLOPT_POST, true);
+	curl_setopt($curl,CURLOPT_POSTFIELDS, $fields_string);
+	curl_setopt($curl,CURLOPT_RETURNTRANSFER, true); 
+	$resp = curl_exec($curl);
+
+	$data = json_decode($resp, true);
+	//Return true if refresh-token is obtained
+	if(isset($data["refresh"])){
+		return true;
+	}
+	return false;
+	//validLogin($_POST['phone'], $_POST['password']);
+}
+
+//For debugging
+// function console_log( $data ) {
+//     $output  = "<script>console.log( '";
+//     $output .= json_encode(print_r($data, true));
+//     $output .= "' );</script>";
+//     echo $output;
+// }
+
+function main(){
+	 $data = getMemberData();
+	 if (JSON_ERROR_NONE !== json_last_error()) {
+		return "Looks like something is wrong with the configuration!";
+    }
+	//User with correct premissions has to be configured before use
+	if(!get_option('login_username')){
+		return "Looks like something is wrong with the configuration!";
+	}
+
+	if ( isset( $_POST['registerForm'] ) ) {
+
+		if(validLogin($_POST['phone'], $_POST['password'])){
+			foreach ($data["results"] as $user){
+		
+				$phone_number = $user["phone_number"];
+		
+				if($_POST['phone'] == $phone_number){
+					//If NTNUI membership is active (expires after today)
+					if ($user["ntnui_contract_expiry_date"] > date("Y-m-d")) {
+						//Access to rent
+						//Login to user with the correct permissions
+						$user_login = get_option('login_username'); 
+						$user = get_userdatabylogin($user_login);
+						$user_id = $user->ID; 
+						wp_set_current_user($user_id, $user_login);
+						wp_set_auth_cookie($user_id); 
+						do_action('wp_login', $user_login); 
+
+						$response = "Login valid";
+						//Redirect to home page on login
+						header("location:". home_url());
+					}
+				}
+			}
+			if(!isset($response)){
+				$response = "No valid membership in NTNUI/Seiling";
+			}
+		}
+		else{
+			$response = "Invalid username or password";
+		}
+		}
+    
+	ob_start();
+	?>
+	 <style> 
+	 	#NTNUI-login {
+			width: 100%;
+			text-align: center;
+		 }
+
+		input[type=text], input[type=password]  {
+		max-width: 30vw;
+		padding: 12px 20px;
+		margin: 8px 0;
+		box-sizing: border-box;
+		border: 3px solid #ccc;
+		-webkit-transition: 0.5s;
+		transition: 0.5s;
+		outline: none;
+		}
+
+		input[type=text]:focus, input[type=password]:focus {
+		border: 3px solid #555;
+		}
+
+		input[type=submit] {
+			background-color: rgb(0,128,55);
+			border: none;
+			color: white;
+			padding: 15px 25px;
+			text-align: center;
+			text-decoration: none;
+			display: inline-block;
+			font-size: 16px;
+			margin: 4px 2px;
+			cursor: pointer;
+		}
+	 </style>
+    <form method = "post" id= "NTNUI-login">
+        <input type="text" name="phone" autocomplete="off" placeholder="Phone (+0012345678)"><br>
+        <input type="password" name="password" autocomplete="off" placeholder="password"><br>
+        <input type="submit" name="registerForm" value="Log in">
+		<?php if(isset($response)){ echo $response; } ?>
+    </form>
+    <?php
+	return ob_get_clean();
+}
+add_shortcode( 'NTNUI-login', 'main' );
+?>
