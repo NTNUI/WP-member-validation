35f3269d5f1dfa04f5691d95a680c4457867f67a
Eivind Dalholt <eividalh@stud.ntnu.no>
2022-05-31 01:17:02 +0200

    admin page updated for coustom role

--- member-validation.php
+++ member-validation.php
@@ -3,7 +3,7 @@
  * Plugin Name: NTNUI Member Integration
  * Description: Log users in to a given account if they are registerd members of given group and NTNUI (Together with e.g. WPFront User Role Editor users can get permission to view restricted pages etc.)
  * Author: Eivind Dalholt
- * Version: 1.1.0
+ * Version: 2.0.0
  */
 
 require_once dirname( __FILE__ ) .'/NTNUI-login.php';
@@ -30,8 +30,8 @@ function add_config_data_callback() { ?>
         <form action="<?php echo esc_url( admin_url('admin-post.php') ); ?>" method="POST">
             <h3>Group Slug (Remember uppercase on first letter)</h3>
             <input type="text" name="group_slug" placeholder="Enter group slug" <?php if(get_option('group_slug') != ""){echo 'value="'. get_option('group_slug'). '"';} ?>>
-            <h3>WP username for logged in users (existing user)</h3>
-            <input type="text" name="login_username" placeholder="Enter username (existing user)" <?php if(get_option('login_username') != ""){echo 'value="'. get_option('login_username'). '"';} ?>>
+            <h3>Role for new users (Permission control)</h3>
+            <input type="text" name="access_type" placeholder="Enter role for new users" <?php echo 'value="'. get_option('access_type', 'subscriber'). '"'; ?>>
             <input type="hidden" name="action" value="process_form">			 
             <br><br>
             <input type="submit" name="submit" id="submit" class="update-button button button-primary" value="Update"  />
@@ -42,9 +42,9 @@ function add_config_data_callback() { ?>
 
 // Submit functionality
 function submit_data() {
-    if (isset($_POST['group_slug']) && isset($_POST['login_username'])) {
+    if (isset($_POST['group_slug']) && isset($_POST['access_type'])) {
         $group_slug = sanitize_text_field( $_POST['group_slug'] );
-        $login_username = sanitize_text_field( $_POST['login_username'] );
+        $access_type = strtolower(sanitize_text_field( $_POST['access_type'] ));
 
         $group_exists = get_option('group_slug');
         if (!empty($group_slug) && !empty($group_exists)) {
@@ -53,11 +53,11 @@ function submit_data() {
             add_option('group_slug', $group_slug);
         }
 
-        $username_exists = get_option('login_username');
-        if (!empty($login_username) && !empty($username_exists)) {
-            update_option('login_username', $login_username);
+        $access_type_exists = get_option('access_type');
+        if (!empty($access_type) && !empty($access_type_exists)) {
+            update_option('access_type', $access_type);
         } else {
-            add_option('login_username', $login_username);
+            add_option('access_type', $access_type);
         }
     }
     wp_redirect($_SERVER['HTTP_REFERER']);
