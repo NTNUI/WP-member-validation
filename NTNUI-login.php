eba9945bddd8e04d0d6f55984746d7603a2f3bf8
Eivind Dalholt <eividalh@stud.ntnu.no>
2023-01-22 23:38:33 +0100

    only phone number as username

--- NTNUI-login.php
+++ NTNUI-login.php
@@ -52,7 +52,7 @@ function main(){
 				if($membership["group"] == get_option('group_slug') && $memberInfo["contract_expiry_date"] > date("Y-m-d")){
 					//Access to rent
 					//Register user if not in database
-					$username = strtolower(trim($phone, "+").$memberInfo["last_name"]);
+					$username = trim($phone, "+");
 					if ( !username_exists($username)  && !email_exists($memberInfo["email"]) ) {
 						$userdata = array(
 							'user_login' =>  $username,
